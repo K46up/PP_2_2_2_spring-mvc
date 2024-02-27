f0ce37010f2aea362d1e221985abdd71f16bd653
Meu3 <k46up@yandex.ru>
2024-02-27 05:41:35 +0300

    Ready 0.1.0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,15 +3,11 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import web.model.Car;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
@@ -24,13 +20,7 @@ public class CarController {
     }
 
     @GetMapping
-    public String printListCar(Model model) {
-        model.addAttribute("cars", carServiceImpl.getCars());
-        return "cars";
-    }
-
-    @GetMapping(value = "/cars?count={count}")
-    public String printListCarForCount(@PathVariable("count") int count, Model model) {
+    public String printListCar(@RequestParam(value = "count", defaultValue = "0") int count, Model model) {
         model.addAttribute("cars", carServiceImpl.getListCarForCount(count));
         return "cars";
     }
